8502fb62c6972afdc02757c8d4fa6f3cc0d3fdd9
sijune <sijune0525@gmail.com>
2021-06-12 15:51:11 +0900

    deploy react project

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css"
+
+function About() {
+    return (
+    <div className="about__container">
+        <span> about page </span>
+        <span> Movie App</span>
+    </div>
+    );
+}
+
+export default About;
